46c00c04380e42ddd84154b7dab34fef5fdcc4b1
hamad <hamadali77115033@gmail.com>
2019-06-29 22:27:43 +0500

    updated code

---
+++ todayjs.js
@@ -0,0 +1,24 @@
+ var name ="Muhammad Hamad";
+ var fathername ="Muhamamd Shafiq";
+    if(name=="Muhammad Hamad" )
+    {
+
+          alert("THIS IS MY FIRST iF")
+          if(fathername ==="Rpo"){
+            alert("THIS IS My 2nd iF")
+        
+       
+          }
+          else{
+            alert("THIS IS MY 2nd ELSE")
+
+
+          }
+
+
+    }
+    else{
+ alert("THIS IS MY FIRST ELSE")
+
+
+    }
