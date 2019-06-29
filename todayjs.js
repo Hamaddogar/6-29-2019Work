ec89f07827dd3d816d0930f96092b4ba764df5ce
hamad <hamadali77115033@gmail.com>
2019-06-29 22:49:30 +0500

    Updated code

--- todayjs.js
+++ todayjs.js
@@ -1,24 +1,52 @@
- var name ="Muhammad Hamad";
- var fathername ="Muhamamd Shafiq";
-    if(name=="Muhammad Hamad" )
-    {
+// var name1=prompt("Enter your Name1");
+// var name2=prompt("Enter your Name2");
+// var name3=prompt("Enter your Name3");
+// var name4=prompt("Enter your Name4");
+// var nameArraynew =[name1,name2,name3,name4];
+// console.log(nameArraynew)
 
-          alert("THIS IS MY FIRST iF")
-          if(fathername ==="Rpo"){
-            alert("THIS IS My 2nd iF")
+
+
+
+
+
+
+
+var nameArray=[];
+for ( var i=0; i<5; i++)
+{
+ var name = prompt("Enter your Name")
+ nameArray.push(name)
+
+}
+ console.log(nameArray)
+
+
+
+
+
+
+//  var name ="Muhammad Hamad";
+//  var fathername ="Muhamamd Shafiq";
+//     if(name=="Muhammad Hamad" )
+//     {
+
+//           alert("THIS IS MY FIRST iF")
+//           if(fathername ==="Rpo"){
+//             alert("THIS IS My 2nd iF")
         
        
-          }
-          else{
-            alert("THIS IS MY 2nd ELSE")
+//           }
+//           else{
+//             alert("THIS IS MY 2nd ELSE")
 
 
-          }
+//           }
 
 
-    }
-    else{
- alert("THIS IS MY FIRST ELSE")
+//     }
+//     else{
+//  alert("THIS IS MY FIRST ELSE")
 
 
-    }
+//     }
